ce9c95475170d4ee860b031b9dd0a3632661276b
sumca252 <ismail.ali9315@gmail.com>
2022-12-07 16:06:27 +0100

    loads fake data

---
+++ db/sql/dml.sql
@@ -0,0 +1,157 @@
+--
+-- role
+--
+
+LOAD DATA LOCAL INFILE 'csv/role.csv' 
+INTO TABLE role
+CHARACTER SET utf8
+FIELDS 
+    TERMINATED BY ','
+    ENCLOSED BY '"'
+LINES
+    TERMINATED BY '\n'
+IGNORE 1 LINES
+;
+
+--
+-- user
+--
+
+LOAD DATA LOCAL INFILE 'csv/user.csv'
+INTO TABLE user
+CHARACTER SET utf8
+FIELDS 
+    TERMINATED BY ','
+    ENCLOSED BY '"'
+LINES
+    TERMINATED BY '\n'
+IGNORE 1 LINES
+;
+
+--
+-- customer
+--
+LOAD DATA LOCAL INFILE 'custcsv/omer.csv'
+INTO TABLE customer
+CHARACTER SET utf8
+FIELDS 
+    TERMINATED BY ','
+    ENCLOSED BY '"'
+LINES
+    TERMINATED BY '\n'
+IGNORE 1 LINES
+;
+
+--
+-- account
+--
+LOAD DATA LOCAL INFILE 'acccsv/ount.csv'
+INTO TABLE account
+CHARACTER SET utf8
+FIELDS 
+    TERMINATED BY ','
+    ENCLOSED BY '"'
+LINES
+    TERMINATED BY '\n'
+IGNORE 1 LINES
+;
+
+--
+-- status
+--
+LOAD DATA LOCAL INFILE 'stcsv/atus.csv'
+INTO TABLE status
+CHARACTER SET utf8
+FIELDS 
+    TERMINATED BY ','
+    ENCLOSED BY '"'
+LINES
+    TERMINATED BY '\n'
+IGNORE 1 LINES
+;
+
+--
+-- price
+--
+LOAD DATA LOCAL INFILE 'pcsv/rice.csv'
+INTO TABLE price
+CHARACTER SET utf8
+FIELDS 
+    TERMINATED BY ','
+    ENCLOSED BY '"'
+LINES
+    TERMINATED BY '\n'
+IGNORE 1 LINES
+;
+
+--
+-- scooter
+--
+LOAD DATA LOCAL INFILE 'scocsv/oter.csv'
+INTO TABLE scooter
+CHARACTER SET utf8
+FIELDS 
+    TERMINATED BY ','
+    ENCLOSED BY '"'
+LINES
+    TERMINATED BY '\n'
+IGNORE 1 LINES
+;
+
+--
+-- zone
+--
+LOAD DATA LOCAL INFILE 'csv/zone.csv'
+INTO TABLE zone
+CHARACTER SET utf8
+FIELDS 
+    TERMINATED BY ','
+    ENCLOSED BY '"'
+LINES
+    TERMINATED BY '\n'
+IGNORE 1 LINES
+;
+
+--
+-- city
+--
+LOAD DATA LOCAL INFILE 'csv/city.csv'
+INTO TABLE city
+CHARACTER SET utf8
+FIELDS 
+    TERMINATED BY ','
+    ENCLOSED BY '"'
+LINES
+    TERMINATED BY '\n'
+IGNORE 1 LINES
+;
+
+--
+-- payment
+--
+LOAD DATA LOCAL INFILE 'paycsv/ment.csv'
+INTO TABLE payment
+CHARACTER SET utf8
+FIELDS 
+    TERMINATED BY ','
+    ENCLOSED BY '"'
+LINES
+    TERMINATED BY '\n'
+IGNORE 1 LINES
+;
+
+--
+-- logs
+--
+-- LOAD DATA LOCAL INFILE 'csv/logs.csv'
+-- INTO TABLE logs
+-- CHARACTER SET utf8
+-- FIELDS 
+--     TERMINATED BY ','
+--     ENCLOSED BY '"'
+-- LINES
+--     TERMINATED BY '\n'
+-- IGNORE 1 LINES
+-- ;
+
+
