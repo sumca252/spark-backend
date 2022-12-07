5bebfd6cf748de1985c2090e5bd09251a1c02885
sumca252 <ismail.ali9315@gmail.com>
2022-12-07 16:05:25 +0100

    setup database

---
+++ db/sql/setup.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS sparkdb;
+
+CREATE DATABASE IF NOT EXISTS sparkdb;
+
+USE sparkdb;
+
+DROP USER IF EXISTS 'spark' @'%';
+
+CREATE USER 'spark' @'%' IDENTIFIED WITH mysql_native_password BY 'password';
+
+GRANT ALL PRIVILEGES ON *.* TO 'spark' @'%';
+
+SET
+    GLOBAL local_infile = 1;
